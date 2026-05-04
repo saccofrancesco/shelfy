da2ea30b192924241da11f59cf9b7811a3705342
Francesco Sacco <francescosacco.github@gmail.com>
2026-05-04 14:27:17 +0200

    feat: add BookCard component to display book information

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import Navbar from "./components/Navbar";
+import BookCard from "./components/BookCard";
+
+const book = {
+  title: "Delitto e castigo",
+  author: "Fëdor Dostoevskij",
+  year: 1866,
+  genre: "Psicologico",
+  description:
+    "La tormentata coscienza di un giovane che ha commesso un omicidio.",
+};
 
 function App() {
   return (
     <>
       <Navbar />
+      <BookCard book={book} />
     </>
   );
 }
