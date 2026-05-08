177a3ea9125e77c66b9cda7d8628be49e6ce718b
Francesco Sacco <francescosacco.github@gmail.com>
2026-05-08 10:50:27 +0200

    feat(constants): add SEARCH_FIELDS and BOOK_GENRES constants

---
+++ frontend/src/constants/books.js
@@ -0,0 +1,22 @@
+export const SEARCH_FIELDS = [
+  { value: "title", label: "Title" },
+  { value: "author", label: "Author" },
+];
+
+export const BOOK_GENRES = [
+  "Adventure",
+  "Coming-of-age",
+  "Drama",
+  "Dystopian",
+  "Fantasy",
+  "Fairy Tale",
+  "Historical",
+  "Novel",
+  "Philosophical",
+  "Post-apocalyptic",
+  "Psychological",
+  "Satire",
+  "Science Fiction",
+  "Thriller",
+  "Other",
+];
